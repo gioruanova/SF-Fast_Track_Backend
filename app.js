7651d0872443fb949461cca8c3930103dcc11b68
Jorge Ruanova <jruanova.dev@gmail.com>
2025-07-17 22:42:25 -0300

    test 10

--- app.js
+++ app.js
@@ -35,6 +35,13 @@ app.get("/ping", (req, res) => {
 });
 
 
+console.log(`Database Name: ${process.env.DB_NAME}`);
+console.log(`Database User: ${process.env.DB_USER}`);
+console.log(`Database Password: ${process.env.DB_PASSWORD}`);
+console.log(`Database Host: ${process.env.DB_HOST}`);
+console.log(`Database Port: ${process.env.DB_PORT}`);
+
+
 // =====================================================================
 // 404 fallback
 app.use((req, res) => {
